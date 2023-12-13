1629b13118ecec2df51049bac21d8d617a366a0c
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-13 09:56:35 +0800

    chore: simplier

--- src-tauri/src/desktop.rs
+++
@@ -1,8 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-use tauri_deno_mobile_sample::AppBuilder;
-
-pub fn main() {
-    AppBuilder::new().run();
-}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,39 @@
-#[cfg(desktop)]
-mod desktop;
+use tauri::App;
 
+pub type SetupHook = Box<dyn FnOnce(&mut App) -> Result<(), Box<dyn std::error::Error>> + Send>;
+
+#[derive(Default)]
+pub struct AppBuilder {
+    setup: Option<SetupHook>,
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+impl AppBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn setup<F>(mut self, setup: F) -> Self
+    where
+        F: FnOnce(&mut App) -> Result<(), Box<dyn std::error::Error>> + Send + 'static,
+    {
+        self.setup.replace(Box::new(setup));
+        self
+    }
+
+    pub fn run(self) {
+        tauri::Builder::default()
+            .invoke_handler(tauri::generate_handler![greet])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
+}
 fn main() {
-    #[cfg(desktop)]
-    desktop::main();
+    AppBuilder::new().run();
 }
